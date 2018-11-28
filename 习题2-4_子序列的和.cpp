e13ad6fb5d514c19e72ef49a7ca8b3ba1d3c0730
SKB <973689813@qq.com>
2018-11-29 00:18:42 +0800

    Update 习题2-4_子序列的和.cpp

--- 习题2-4_子序列的和.cpp
+++ 习题2-4_子序列的和.cpp
@@ -8,7 +8,7 @@ int main(){
     double sum;
     while((cin>>n>>m)&&n&&m){
         sum=0;
-        for(int i=n;i<=m;i++)sum+=1.0/((double)i*(double)i);
+        for(int i=n;i<=m;i++)sum+=1.0/((double)i*i);
         printf("Case %d: %.5f\n",count,sum);
         count++;
     }
