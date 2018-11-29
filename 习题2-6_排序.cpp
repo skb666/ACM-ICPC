74bf5046014e3f256b9a9d68ee3ecb1e15184214
SKB <973689813@qq.com>
2018-11-29 18:26:33 +0800

    Update 习题2-6_排序.cpp

--- 习题2-6_排序.cpp
+++ 习题2-6_排序.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-    int t1,t2,t3,e,f,g,h,i,j;
+    int t1,t2,t3,d,e,f,g,h,i;
     for(int a=1;a<=9;a++){
         for(int b=1;b<=9;b++){
             for(int c=1;c<=9;c++){
@@ -13,14 +13,14 @@ int main(){
                 t3=t1*3;
                 if(t2>999||t3>999)continue;
                 else{
-                    e=t2/100;
-                    f=(t2/10)%10;
-                    g=t2%10;
-                    h=t3/100;
-                    i=(t3/10)%10;
-                    j=t3%10;
-                    if(e!=f&&e!=g&&e!=h&&e!=i&&e!=j&&f!=g&&f!=h&&f!=i&&f!=j&&g!=h&&g!=i&&g!=j&&h!=i&&h!=j&&i!=j){
-                        cout<<a<<b<<c<<' '<<e<<f<<g<<' '<<h<<i<<j<<endl;
+                    d=t2/100;
+                    e=(t2/10)%10;
+                    f=t2%10;
+                    g=t3/100;
+                    h=(t3/10)%10;
+                    i=t3%10;
+                    if(d!=e&&d!=f&&d!=g&&d!=h&&d!=i&&e!=f&&e!=g&&e!=h&&e!=i&&f!=g&&f!=h&&f!=i&&g!=h&&g!=i&&h!=i){
+                        cout<<a<<b<<c<<' '<<d<<e<<f<<' '<<g<<h<<i<<endl;
                     }
                 }
             }
