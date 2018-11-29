b89bbcaecc683ddaeada53b70f2cee2cbaaed030
SKB <973689813@qq.com>
2018-11-29 17:37:25 +0800

    Update 习题2-5_分数化小数.cpp

--- 习题2-5_分数化小数.cpp
+++ 习题2-5_分数化小数.cpp
@@ -4,19 +4,19 @@
 using namespace std;
 
 int main(){
-    int a,b,c,kase = 0;
+    int a,b,c,count=1;
     while ((cin>>a>>b>>c)&&!(a==0&&b==0&&c==0)){
-        kase++;
-        printf("Case: %d: %d.", kase, a/b);
-        a %= b;
-        for (int i = 0; i < c-1; ++i) {
-            a *= 10;
-            printf("%d", a/b);
-            a %= b;
+        printf("Case: %d: %d.",count,a/b);
+        a%=b;
+        for(int i=0;i<c-1;i++) {
+            a*=10;
+            printf("%d",a/b);
+            a%=b;
         }
-        int more = ((a*10)%b * 10) / b;
-        if (more >= 5)printf("%d\n", (a*10)/b + 1);
-        else printf("%d\n", (a*10)/b);
+        int more=((a*10)%b*10)/b;
+        if (more>=5)printf("%d\n",(a*10)/b+1);
+        else printf("%d\n",(a*10)/b);
+        count++;
     }
     return 0;
 }
