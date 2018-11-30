530ea2bfe9f65e4ca57dd37f204439fb6c8f276c
SKB <973689813@qq.com>
2018-11-30 15:14:02 +0800

    Update 例题1-1_圆柱体的表面积.cpp

--- 算法竞赛入门经典(第二版)/第1章/例题/例题1-1_圆柱体的表面积.cpp
+++ 算法竞赛入门经典(第二版)/第1章/例题/例题1-1_圆柱体的表面积.cpp
@@ -1,1 +1,11 @@
 #include<iostream>
+#include<cstdio>
+using namespace std;
+
+#define PI 3.1415926535
+int main(){
+    double r,h;
+    cin>>r>>h;
+    printf("Area = %.3lf\n",(2*PI*r*h+2*PI*r*r));
+    return 0;
+}
